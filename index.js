7790f377bd0f35457980792b98e367aaba86365e
hoonkim <hoonk9387@gmail.com>
2022-04-04 19:16:46 -0500

    Completed assignment.

--- index.js
+++ index.js
@@ -10,7 +10,52 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+/* Starter code. */
+// const titleCased = () => {
+//   return tutorials
+// }
 
+/* IT WORKS!!! */
 const titleCased = () => {
-  return tutorials
+    // Callback
+    function callback(element) {
+      // Split string into array of separate strings. 
+      element = element.split(' ')
+      // Loop through array.
+      for (let i = 0; i < element.length; i++) {
+          element[i] = element[i].charAt(0).toUpperCase() + element[i].slice(1)
+      }
+      return element.join(' ')        
+  }
+  // Map function
+  return tutorials.map(callback)
+  // return tutorials
 }
+
+/* WORKS IN DEV TOOLS BUT NOT WITH NPM TEST. */
+// function titleCased(tutorials) {
+//   // Callback
+//   function callback(element) {
+//       // Split string into array of separate strings. 
+//       element = element.split(' ')
+//       // Loop through array.
+//       for (let i = 0; i < element.length; i++) {
+//           element[i] = element[i].charAt(0).toUpperCase() + element[i].slice(1)
+//       }
+//       return element.join(' ')        
+//   }
+
+//   // Map function
+//   return tutorials.map(callback)
+// }
+
+/* THIS WORKS. */
+// function titleCased() {
+//   const fixTutorials = tutorials.map(tSplit => {
+//     const sentenceArr = tSplit.split(" ").map(sa => {
+//       return sa.charAt(0).toUpperCase() + sa.slice(1)
+//     }).join(" ")
+//     return sentenceArr;
+//   })
+// return fixTutorials
+// }
